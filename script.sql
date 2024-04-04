cf9f77851a8c5c71d8eb5aaf5c3f293621abeff1
arrrnold <tm02arnold@gmail.com>
2024-04-04 15:28:14 -0600

    Fixed imagen vacia por default

--- script.sql
+++ script.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS usuario
     clave           VARCHAR(10)  NOT NULL,
     nombre_completo VARCHAR(100) NOT NULL,
     email           VARCHAR(100) NOT NULL,
-    imagen_perfil   BLOB         NOT NULL,
+    imagen_perfil   BLOB         DEFAULT NULL, # por defecto no tiene foto de perfil
     pedido_id       INT          DEFAULT NULL,                       # puede ser nulo si el usuario no ha hecho un pedido
     PRIMARY KEY (id),
     INDEX fk_usuario_pedido1_idx (pedido_id ASC),
