b930a8681577d8734c4066f012e23982ce609276
arrrnold <tm02arnold@gmail.com>
2024-07-05 07:23:29 -0600

    Cambios finales

--- pruebas.sql
+++ pruebas.sql
@@ -1,115 +1,122 @@
-# # SCRIPT QUE GENERA LA BD "cafeteria" Y SUS TABLAS
-#
-# # drop si ya existe
-# DROP SCHEMA IF EXISTS cafeteria;
-#
-# CREATE SCHEMA IF NOT EXISTS cafeteria DEFAULT CHARACTER SET utf8;
-# USE cafeteria;
-#
-# CREATE TABLE IF NOT EXISTS pedido
-# (
-#     id           INT          NOT NULL AUTO_INCREMENT UNIQUE,
-#     fecha        DATE         NOT NULL,
-#     cant_a_pagar INT          NULL, # puede ser nulo si el pedido se paga con tarjeta
-#     propina      INT          NOT NULL,
-#     total        INT          NOT NULL,
-#     pagado       TINYINT      NOT NULL,
-#     preferencias VARCHAR(500) NOT NULL,
-#     ubicacion    VARCHAR(500) NOT NULL,
-#     folio        VARCHAR(45)  NULL, # puede ser nulo si el pedido se en efectivo
-#     estado       VARCHAR(45)  NOT NULL,
-#     PRIMARY KEY (id)
-# ) ENGINE = InnoDB;
-#
-# CREATE TABLE IF NOT EXISTS usuario
-# (
-#     id              INT          NOT NULL AUTO_INCREMENT UNIQUE,
-#     rol             VARCHAR(13)  NOT NULL DEFAULT 'usuario', # puede ser usuario o administrador
-#     clave           VARCHAR(10)  NOT NULL,
-#     nombre_completo VARCHAR(100) NOT NULL,
-#     email           VARCHAR(100) NOT NULL,
-#     imagen_perfil   BLOB         NOT NULL,
-#     pedido_id       INT          NULL,                       # puede ser nulo si el usuario no ha hecho un pedido
-#     PRIMARY KEY (id),
-#     INDEX fk_usuario_pedido1_idx (pedido_id ASC),
-#     CONSTRAINT fk_usuario_pedido1 FOREIGN KEY (pedido_id) REFERENCES pedido (id) ON DELETE NO ACTION ON UPDATE NO ACTION
-# ) ENGINE = InnoDB;
-#
-#
-# CREATE TABLE IF NOT EXISTS mensaje
-# (
-#     id        INT           NOT NULL AUTO_INCREMENT UNIQUE,
-#     fecha     DATE          NOT NULL,
-#     hora      TIME          NOT NULL,
-#     contenido VARCHAR(1000) NOT NULL,
-#     PRIMARY KEY (id)
-# ) ENGINE = InnoDB;
-#
-# CREATE TABLE IF NOT EXISTS producto
-# (
-#     id            INT         NOT NULL AUTO_INCREMENT UNIQUE,
-#     precio        INT         NOT NULL CHECK (precio > 0),
-#     cantidad      INT         NOT NULL check ( cantidad >= 0),
-#     recomendacion TINYINT     NOT NULL,           # 1 recomendado, 0 no recomendado
-#     imagen        BLOB        NOT NULL,
-#     categoria     VARCHAR(20) NOT NULL,           # solo: 'Bebida abierta', 'Bebida cerrada' y 'Alimentos'
-#     nombre        VARCHAR(50) NOT NULL,
-#     visible       TINYINT     NOT NULL DEFAULT 1, # 1 visible, 0 no visible
-#     PRIMARY KEY (id)
-# ) ENGINE = InnoDB;
-#
-# CREATE TABLE IF NOT EXISTS pedido_tiene_producto
-# (
-#     pedido_id   INT NOT NULL,
-#     producto_id INT NOT NULL,
-#     cantidad    INT NOT NULL,
-#     precio      INT NOT NULL CHECK (precio > 0), # precio del producto en el momento de la compra
-#     PRIMARY KEY (pedido_id, producto_id),
-#     INDEX fk_pedido_has_producto_producto1_idx (producto_id ASC),
-#     INDEX fk_pedido_has_producto_pedido1_idx (pedido_id ASC),
-#     CONSTRAINT fk_pedido_has_producto_pedido1 FOREIGN KEY (pedido_id) REFERENCES pedido (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
-#     CONSTRAINT fk_pedido_has_producto_producto1 FOREIGN KEY (producto_id) REFERENCES producto (id) ON DELETE NO ACTION ON UPDATE NO ACTION
-# ) ENGINE = InnoDB;
-#
-# CREATE TABLE IF NOT EXISTS usuario_tiene_mensaje
-# (
-#     usuario_id INT NOT NULL,
-#     mensaje_id INT NOT NULL,
-#     PRIMARY KEY (usuario_id, mensaje_id),
-#     INDEX fk_usuario_has_mensaje_mensaje1_idx (mensaje_id ASC),
-#     INDEX fk_usuario_has_mensaje_usuario1_idx (usuario_id ASC),
-#     CONSTRAINT fk_usuario_has_mensaje_usuario1 FOREIGN KEY (usuario_id) REFERENCES usuario (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
-#     CONSTRAINT fk_usuario_has_mensaje_mensaje1 FOREIGN KEY (mensaje_id) REFERENCES mensaje (id) ON DELETE NO ACTION ON UPDATE NO ACTION
-# ) ENGINE = InnoDB;
-#
+/*
+# SCRIPT QUE GENERA LA BD "cafeteria" Y SUS TABLAS
 
+# drop si ya existe
+DROP SCHEMA IF EXISTS cafeteria;
+
+CREATE SCHEMA IF NOT EXISTS cafeteria DEFAULT CHARACTER SET utf8;
+USE cafeteria;
+
+CREATE TABLE IF NOT EXISTS mensajes
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    fecha     DATETIME     NOT NULL,
+    hora      TIME         NOT NULL,
+    contenido VARCHAR(255) NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS pedidos
+(
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    fecha        DATETIME     NOT NULL,
+    cant_a_pagar INT          DEFAULT NULL, # puede ser nulo si el pedido se paga con tarjeta
+    propina      INT          NOT NULL,
+    total        INT          NOT NULL,
+    pagado       TINYINT(1)   NOT NULL,
+    preferencias VARCHAR(500) NOT NULL,
+    ubicacion    VARCHAR(500) NOT NULL,
+    folio        VARCHAR(45)  DEFAULT NULL, # puede ser nulo si el pedido se paga en efectivo
+    estado       VARCHAR(45)  NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS productos
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    nombre        VARCHAR(255) NOT NULL,
+    precio        INT          NOT NULL CHECK (precio > 0),
+    cantidad      INT          NOT NULL CHECK (cantidad >= 0),
+    recomendacion VARCHAR(255) NOT NULL, # 1 recomendado, 0 no recomendado
+    imagen        VARCHAR(255) NOT NULL,
+    categoria     VARCHAR(255) NOT NULL, # solo: 'bebida abiertas', 'bebida cerradas' y 'alimentos'
+    visible       TINYINT(1)   DEFAULT 1 # 1 visible, 0 no visible
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS pedido_tiene_producto
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    pedido_id   INT NOT NULL,
+    producto_id INT NOT NULL,
+    cantidad    INT NOT NULL,
+    precio      INT NOT NULL CHECK (precio > 0), # precio del producto en el momento de la compra
+    CONSTRAINT pedido_tiene_producto_ibfk_1 FOREIGN KEY (pedido_id) REFERENCES pedidos (id) ON UPDATE CASCADE,
+    CONSTRAINT pedido_tiene_producto_ibfk_2 FOREIGN KEY (producto_id) REFERENCES productos (id) ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+CREATE INDEX pedido_id ON pedido_tiene_producto (pedido_id);
+CREATE INDEX producto_id ON pedido_tiene_producto (producto_id);
+
+CREATE TABLE IF NOT EXISTS usuarios
+(
+    id                        INT AUTO_INCREMENT PRIMARY KEY,
+    rol                       VARCHAR(13) DEFAULT 'usuario' NOT NULL, # puede ser usuario o administrador
+    clave                     VARCHAR(255) NOT NULL,
+    nombre_completo           VARCHAR(100) NOT NULL,
+    email                     VARCHAR(100) NOT NULL,
+    imagen_perfil             VARCHAR(255) DEFAULT NULL, # por defecto no tiene foto de perfil
+    token_de_restablecimiento VARCHAR(100) DEFAULT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS usuario_tiene_mensaje
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    mensaje_id INT NOT NULL,
+    CONSTRAINT usuario_tiene_mensaje_ibfk_1 FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON UPDATE CASCADE,
+    CONSTRAINT usuario_tiene_mensaje_ibfk_2 FOREIGN KEY (mensaje_id) REFERENCES mensajes (id) ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+CREATE INDEX mensaje_id ON usuario_tiene_mensaje (mensaje_id);
+CREATE INDEX usuario_id ON usuario_tiene_mensaje (usuario_id);
+
+CREATE TABLE IF NOT EXISTS usuarios_tienen_pedidos
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    pedido_id  INT NOT NULL,
+    CONSTRAINT usuarios_tienen_pedidos_ibfk_1 FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT usuarios_tienen_pedidos_ibfk_2 FOREIGN KEY (pedido_id) REFERENCES pedidos (id)
+) ENGINE = InnoDB;
+
+CREATE INDEX pedido_id ON usuarios_tienen_pedidos (pedido_id);
+CREATE INDEX usuario_id ON usuarios_tienen_pedidos (usuario_id);
+*/
 # 1. Insertar un pedido en efectivo
 INSERT INTO pedidos (fecha, cant_a_pagar, propina, total, pagado, preferencias, ubicacion, folio, estado)
-VALUES ('2021-05-01', 100, 20, 120, 1, 'sin azúcar', 'Aula 123', null, 'entregado');
+VALUES ('2021-05-01 00:00:00', 100, 20, 120, 1, 'sin azúcar', 'Aula 123', NULL, 'entregado');
 
 # 2. Insertar un pedido con tarjeta
 INSERT INTO pedidos (fecha, cant_a_pagar, propina, total, pagado, preferencias, ubicacion, folio, estado)
-VALUES ('2021-05-01', null, 20, 120, 1, 'sin cebolla', 'Lab. de computo', '1234567890', 'entregado');
+VALUES ('2021-05-01 00:00:00', NULL, 20, 120, 1, 'sin cebolla', 'Lab. de computo', '1234567890', 'entregado');
 
 # 3. Insertar un usuario
-INSERT INTO usuarios (rol, clave, nombre_completo, email, imagen_perfil, pedido_id)
-VALUES ('usuario', '123456', 'Juan Pérez', 'juan@mail.com', 'imagen', null);
+INSERT INTO usuarios (rol, clave, nombre_completo, email, imagen_perfil)
+VALUES ('usuario', '123456', 'Juan Pérez', 'juan@mail.com', 'imagen');
 
 # 3.1 Insertar un administrador
-INSERT INTO usuarios (rol, clave, nombre_completo, email, imagen_perfil, pedido_id)
-VALUES ('administrador', '123456', 'Pedro Pérez', 'pedro@mail.com', 'imagen', null);
+INSERT INTO usuarios (rol, clave, nombre_completo, email, imagen_perfil)
+VALUES ('administrador', '123456', 'Pedro Pérez', 'pedro@mail.com', 'imagen');
 
 # 4. Insertar un mensaje
 INSERT INTO mensajes (fecha, hora, contenido)
-VALUES ('2021-05-01', '12:00:00', 'Hola, ¿cómo estás?');
+VALUES ('2021-05-01 00:00:00', '12:00:00', 'Hola, ¿cómo estás?');
 
 # 5. Insertar un producto
-INSERT INTO productos (precio, cantidad, recomendacion, imagen, categoria, nombre, visible)
-VALUES (20, 100, 1, 'imagen', 'Bebida abierta', 'Coca Cola', 1);
+INSERT INTO productos (nombre, precio, cantidad, recomendacion, imagen, categoria, visible)
+VALUES ('Coca Cola', 20, 100, '1', 'imagen', 'Bebidas abiertas', 1);
 
 # otro producto
-INSERT INTO productos (precio, cantidad, recomendacion, imagen, categoria, nombre, visible)
-VALUES (15, 100, 0, 'imagen', 'Bebida cerrada', 'Agua', 1);
+INSERT INTO productos (nombre, precio, cantidad, recomendacion, imagen, categoria, visible)
+VALUES ('Agua', 15, 100, '0', 'imagen', 'Bebidas cerradas', 1);
 
 # 6. Añadir ese producto a un pedido
 INSERT INTO pedido_tiene_producto (pedido_id, producto_id, cantidad, precio)
@@ -120,12 +127,10 @@ INSERT INTO usuario_tiene_mensaje (usuario_id, mensaje_id)
 VALUES (1, 1);
 
 # 7.1 Añadir un pedido a un usuario
-UPDATE usuarios
-SET pedido_id = 1
-WHERE id = 1;
+INSERT INTO usuarios_tienen_pedidos (usuario_id, pedido_id)
+VALUES (1, 1);
 
-/* Pruebas de actualizacion (solo actualizan los usuarios y los productos) */
-
+# Pruebas de actualización (solo actualizan los usuarios y los productos)
 # 8. Actualizar un usuario
 UPDATE usuarios
 SET nombre_completo = 'Juan Pérez Pérez'
@@ -136,16 +141,11 @@ UPDATE productos
 SET precio = 25
 WHERE id = 1;
 
-/* Pruebas de 'eliminacion' de los productos */
-
+# Pruebas de 'eliminación' de los productos (solo se ocultan)
 # 10. Eliminar un producto (solo se oculta)
 UPDATE productos
 SET visible = 0
 WHERE id = 1;
-
-/*
- ******************** 10 PRUEBAS DE CONSULTAS ********************
- */
 
 # 11. Consultar todos los pedidos
 SELECT *
@@ -163,55 +163,66 @@ FROM mensajes;
 SELECT *
 FROM productos;
 
-/* Consultas mas avanzadas */
-
+# Consultas más avanzadas
 # 16. Todos los pedidos de un usuario
 SELECT *
 FROM pedidos
-WHERE id = (SELECT pedido_id FROM usuarios WHERE id = 1);
+WHERE id IN (SELECT pedido_id FROM usuarios_tienen_pedidos WHERE usuario_id = 1);
 
-
--- Agregar más productos al pedido con id = 1
-
--- Producto: Coca Cola (id = 1), agregar 3 unidades al pedido
+# Agregar más productos al pedido con id = 1
+# Producto: Coca Cola (id = 1), agregar 3 unidades al pedido
 INSERT INTO pedido_tiene_producto (pedido_id, producto_id, cantidad, precio)
 VALUES (1, 1, 3, 20);
 
--- Producto: Agua (id = 2), agregar 1 unidad al pedido
+# Producto: Agua (id = 2), agregar 1 unidad al pedido
 INSERT INTO pedido_tiene_producto (pedido_id, producto_id, cantidad, precio)
 VALUES (1, 2, 1, 15);
 
--- Insertar 10 productos con nombres comunes
-INSERT INTO productos (precio, cantidad, recomendacion, imagen, categoria, nombre, visible)
-VALUES (25, 50, 1, 'imagen', 'Bebida abierta', 'Refresco', 1),
-       (30, 30, 1, 'imagen', 'Alimentos', 'Hamburguesa', 1),
-       (10, 100, 0, 'imagen', 'Bebida cerrada', 'Agua', 1),
-       (40, 20, 1, 'imagen', 'Alimentos', 'Pizza', 1),
-       (15, 80, 0, 'imagen', 'Bebida abierta', 'Jugo de Naranja', 1),
-       (50, 10, 1, 'imagen', 'Alimentos', 'Tacos', 1),
-       (12, 90, 0, 'imagen', 'Bebida abierta', 'Café', 1),
-       (35, 40, 1, 'imagen', 'Alimentos', 'Sándwich', 1),
-       (8, 120, 0, 'imagen', 'Bebida cerrada', 'Limonada', 1),
-       (45, 15, 1, 'imagen', 'Alimentos', 'Ensalada', 1);
+# Insertar 10 productos con nombres comunes
+INSERT INTO productos (nombre, precio, cantidad, recomendacion, imagen, categoria, visible)
+VALUES ('Refresco', 25, 50, '1', 'imagen', 'Bebidas abiertas', 1),
+       ('Hamburguesa', 30, 30, '1', 'imagen', 'Alimentos', 1),
+       ('Agua', 10, 100, '0', 'imagen', 'Bebidas cerradas', 1),
+       ('Pizza', 40, 20, '1', 'imagen', 'Alimentos', 1),
+       ('Jugo de Naranja', 15, 80, '0', 'imagen', 'Bebidas abiertas', 1),
+       ('Tacos', 50, 10, '1', 'imagen', 'Alimentos', 1),
+       ('Café', 12, 90, '0', 'imagen', 'Bebidas abiertas', 1),
+       ('Sándwich', 35, 40, '1', 'imagen', 'Alimentos', 1),
+       ('Limonada', 8, 120, '0', 'imagen', 'Bebidas cerradas', 1),
+       ('Ensalada', 45, 15, '1', 'imagen', 'Alimentos', 1);
 
--- Crear nuevos pedidos utilizando los productos recién insertados
-
--- Pedido 2 (pago en efectivo)
+# Crear nuevos pedidos utilizando los productos recién insertados
+# Pedido 2 (pago en efectivo)
 INSERT INTO pedidos (fecha, cant_a_pagar, propina, total, pagado, preferencias, ubicacion, folio, estado)
-VALUES ('2021-05-02', 90, 15, 105, 1, 'sin cebolla', 'Casa', null, 'pendiente');
+VALUES ('2021-05-02 00:00:00', 90, 15, 105, 1, 'sin cebolla', 'Casa', NULL, 'pendiente');
 
--- Asociar productos al Pedido 2 en la tabla pedido_tiene_producto
+# Asociar productos al Pedido 2 en la tabla pedido_tiene_producto
 INSERT INTO pedido_tiene_producto (pedido_id, producto_id, cantidad, precio)
-VALUES (2, 2, 1, 30), -- Hamburguesa
-       (2, 5, 2, 15), -- Jugo de Naranja
+VALUES (2, 2, 1, 30), # Hamburguesa
+       (2, 5, 2, 15), # Jugo de Naranja
        (2, 7, 1, 12);
--- Café
+# Café
 
--- Pedido 3 (pago con tarjeta)
+# Pedido 3 (pago con tarjeta)
 INSERT INTO pedidos (fecha, cant_a_pagar, propina, total, pagado, preferencias, ubicacion, folio, estado)
-VALUES ('2021-05-03', null, 10, 55, 1, 'extra queso', 'Oficina', '9876543210', 'entregado');
+VALUES ('2021-05-03 00:00:00', NULL, 10, 55, 1, 'extra queso', 'Oficina', '9876543210', 'entregado');
 
--- Asociar productos al Pedido 3 en la tabla pedido_tiene_producto
+# Asociar productos al Pedido 3 en la tabla pedido_tiene_producto
 INSERT INTO pedido_tiene_producto (pedido_id, producto_id, cantidad, precio)
-VALUES (3, 3, 3, 10), -- Agua
-       (3, 9, 1, 8); -- Limonada
+VALUES (3, 3, 3, 10), # Agua
+       (3, 9, 1, 8);
+# Limonada
+
+# Insertar productos en el pedido_tiene_producto
+INSERT INTO pedido_tiene_producto (pedido_id, producto_id, cantidad, precio)
+VALUES (1, 1, 2, 20),
+       (1, 2, 1, 15),
+       (1, 2, 1, 15),
+       (1, 3, 3, 10),
+       (1, 4, 1, 40),
+       (1, 5, 2, 15),
+       (1, 6, 1, 50),
+       (1, 7, 1, 12),
+       (1, 8, 1, 35),
+       (1, 9, 1, 8),
+       (1, 10, 1, 45);
--- script.sql
+++ script.sql
@@ -1,84 +1,91 @@
-# SCRIPT QUE GENERA LA BD "cafeteria" Y SUS TABLAS
+-- SCRIPT QUE GENERA LA BD "cafeteria" Y SUS TABLAS
 
-# drop si ya existe
+-- drop si ya existe
 DROP SCHEMA IF EXISTS cafeteria;
 
 CREATE SCHEMA IF NOT EXISTS cafeteria DEFAULT CHARACTER SET utf8;
 USE cafeteria;
 
-CREATE TABLE IF NOT EXISTS pedidos
-(
-    id           INT          NOT NULL AUTO_INCREMENT UNIQUE,
-    fecha        DATE         NOT NULL,
-    cant_a_pagar INT         DEFAULT NULL, # puede ser nulo si el pedido se paga con tarjeta
-    propina      INT          NOT NULL,
-    total        INT          NOT NULL,
-    pagado       TINYINT      NOT NULL,
-    preferencias VARCHAR(500) NOT NULL,
-    ubicacion    VARCHAR(500) NOT NULL,
-    folio        VARCHAR(45) DEFAULT NULL, # puede ser nulo si el pedido se en efectivo
-    estado       VARCHAR(45)  NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS usuarios
-(
-    id                        INT          NOT NULL AUTO_INCREMENT UNIQUE,
-    rol                       VARCHAR(13)  NOT NULL DEFAULT 'usuario', # puede ser usuario o administrador
-    clave                     VARCHAR(250) NOT NULL,
-    nombre_completo           VARCHAR(100) NOT NULL,
-    email                     VARCHAR(100) NOT NULL,
-    imagen_perfil             BLOB                  DEFAULT NULL,      # por defecto no tiene foto de perfil
-    pedido_id                 INT                   DEFAULT NULL,      # puede ser nulo si el usuario no ha hecho un pedido
-    PRIMARY KEY (id),
-    INDEX fk_usuario_pedido1_idx (pedido_id ASC),
-    CONSTRAINT fk_usuario_pedido1 FOREIGN KEY (pedido_id) REFERENCES pedidos (id) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
-
-
 CREATE TABLE IF NOT EXISTS mensajes
 (
-    id        INT           NOT NULL AUTO_INCREMENT UNIQUE,
-    fecha     DATE          NOT NULL,
-    hora      TIME          NOT NULL,
-    contenido VARCHAR(1000) NOT NULL,
-    PRIMARY KEY (id)
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    fecha     DATETIME     NOT NULL,
+    hora      TIME         NOT NULL,
+    contenido VARCHAR(255) NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS pedidos
+(
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    fecha        DATETIME     NOT NULL,
+    cant_a_pagar INT          DEFAULT NULL, -- puede ser nulo si el pedido se paga con tarjeta
+    propina      INT          NOT NULL,
+    total        INT          NOT NULL,
+    pagado       TINYINT(1)   NOT NULL,
+    preferencias VARCHAR(500) NOT NULL,
+    ubicacion    VARCHAR(500) NOT NULL,
+    folio        VARCHAR(45)  DEFAULT NULL, -- puede ser nulo si el pedido se paga en efectivo
+    estado       VARCHAR(45)  NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS productos
 (
-    id            INT         NOT NULL AUTO_INCREMENT UNIQUE,
-    precio        INT         NOT NULL CHECK (precio > 0),
-    cantidad      INT         NOT NULL check ( cantidad >= 0),
-    recomendacion TINYINT     NOT NULL,           # 1 recomendado, 0 no recomendado
-    imagen        BLOB        NOT NULL,
-    categoria     VARCHAR(20) NOT NULL,           # solo: 'bebida abierta', 'bebida cerrada' y 'alimentos'
-    nombre        VARCHAR(50) NOT NULL,
-    visible       TINYINT     NOT NULL DEFAULT 1, # 1 visible, 0 no visible
-    PRIMARY KEY (id)
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    nombre        VARCHAR(255) NOT NULL,
+    precio        INT          NOT NULL CHECK (precio > 0),
+    cantidad      INT          NOT NULL CHECK (cantidad >= 0),
+    recomendacion VARCHAR(255) NOT NULL, -- 1 recomendado, 0 no recomendado
+    imagen        VARCHAR(255) NOT NULL,
+    categoria     VARCHAR(255) NOT NULL, -- solo: 'bebida abierta', 'bebida cerrada' y 'alimentos'
+    visible       TINYINT(1)   DEFAULT 1 -- 1 visible, 0 no visible
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS pedido_tiene_producto
 (
+    id          INT AUTO_INCREMENT PRIMARY KEY,
     pedido_id   INT NOT NULL,
     producto_id INT NOT NULL,
     cantidad    INT NOT NULL,
-    precio      INT NOT NULL CHECK (precio > 0), # precio del producto en el momento de la compra
-    PRIMARY KEY (pedido_id, producto_id),
-    INDEX fk_pedido_has_producto_producto1_idx (producto_id ASC),
-    INDEX fk_pedido_has_producto_pedido1_idx (pedido_id ASC),
-    CONSTRAINT fk_pedido_has_producto_pedido1 FOREIGN KEY (pedido_id) REFERENCES pedidos (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT fk_pedido_has_producto_producto1 FOREIGN KEY (producto_id) REFERENCES productos (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+    precio      INT NOT NULL CHECK (precio > 0), -- precio del producto en el momento de la compra
+    CONSTRAINT pedido_tiene_producto_ibfk_1 FOREIGN KEY (pedido_id) REFERENCES pedidos (id) ON UPDATE CASCADE,
+    CONSTRAINT pedido_tiene_producto_ibfk_2 FOREIGN KEY (producto_id) REFERENCES productos (id) ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+CREATE INDEX pedido_id ON pedido_tiene_producto (pedido_id);
+CREATE INDEX producto_id ON pedido_tiene_producto (producto_id);
+
+CREATE TABLE IF NOT EXISTS usuarios
+(
+    id                        INT AUTO_INCREMENT PRIMARY KEY,
+    rol                       VARCHAR(13) DEFAULT 'usuario' NOT NULL, -- puede ser usuario o administrador
+    clave                     VARCHAR(255) NOT NULL,
+    nombre_completo           VARCHAR(100) NOT NULL,
+    email                     VARCHAR(100) NOT NULL,
+    imagen_perfil             VARCHAR(255) DEFAULT NULL, -- por defecto no tiene foto de perfil
+    token_de_restablecimiento VARCHAR(100) DEFAULT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS usuario_tiene_mensaje
 (
+    id         INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
     mensaje_id INT NOT NULL,
-    PRIMARY KEY (usuario_id, mensaje_id),
-    INDEX fk_usuario_tiene_mensaje_mensaje1_idx (mensaje_id ASC),
-    INDEX fk_usuario_tiene_mensaje_usuario1_idx (usuario_id ASC),
-    CONSTRAINT fk_usuario_tiene_mensaje_usuario1 FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT fk_usuario_tiene_mensaje_mensaje1 FOREIGN KEY (mensaje_id) REFERENCES mensajes (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+    CONSTRAINT usuario_tiene_mensaje_ibfk_1 FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON UPDATE CASCADE,
+    CONSTRAINT usuario_tiene_mensaje_ibfk_2 FOREIGN KEY (mensaje_id) REFERENCES mensajes (id) ON UPDATE CASCADE
 ) ENGINE = InnoDB;
+
+CREATE INDEX mensaje_id ON usuario_tiene_mensaje (mensaje_id);
+CREATE INDEX usuario_id ON usuario_tiene_mensaje (usuario_id);
+
+CREATE TABLE IF NOT EXISTS usuarios_tienen_pedidos
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    pedido_id  INT NOT NULL,
+    CONSTRAINT usuarios_tienen_pedidos_ibfk_1 FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT usuarios_tienen_pedidos_ibfk_2 FOREIGN KEY (pedido_id) REFERENCES pedidos (id)
+) ENGINE = InnoDB;
+
+CREATE INDEX pedido_id ON usuarios_tienen_pedidos (pedido_id);
+CREATE INDEX usuario_id ON usuarios_tienen_pedidos (usuario_id);
 
